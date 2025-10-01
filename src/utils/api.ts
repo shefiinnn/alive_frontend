d61e7c4a03f533b264418c4bbd74bc91700b0982
Muhammed Shefin <shazzshefin@gmail.com>
2025-10-01 12:35:30 +0530

    deploy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'https://alive-backend.onrender.com',
+  baseURL: 'https://alive-backend.onrender.com/api/',
 });
 
 api.interceptors.request.use((config) => {
